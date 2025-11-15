1a3128fb4b5ded1269884564a975616198494204
Nathalia Rodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-11-15 15:08:01 -0600

    chore: Implement backend base

--- src/backend/presentation/app.ts
+++ src/backend/presentation/app.ts
@@ -1,7 +1,12 @@
+
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok" });
+});
 
 export default app;
